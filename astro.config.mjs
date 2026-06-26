78b3a36506549343735714546b6380e8ffa366f2
StephenODea54 <odeastephen1@gmail.com>
2026-06-26 16:38:17 -0400

    add vercel deployment adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";  
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
+import vercel from '@astrojs/vercel';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
   site: "https://spotonmobiledetailing.com",
   integrations: [tailwindcss(), sitemap(), mdx()],
+  adapter: vercel(),
 });
